5c1120e526c6b675b625fa6618a803434c20af4d
Siavash Khalvati <siavash.khalvati@mydrivesolutions.com>
2025-11-12 10:29:20 +0100

    added Prometheus metrics as a middleware for Duration and number of hits

---
+++ pkg/http/metrics.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type PrometheusMiddleware struct {
+	counter  *prometheus.CounterVec
+	duration *prometheus.HistogramVec
+}
+
+func NewMetricMiddleware() *PrometheusMiddleware {
+
+	counter := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "api_requests_total",
+			Help: "A counter for requests to the wrapped handler.",
+		},
+		[]string{"code", "method"},
+	)
+
+	// duration is partitioned by the HTTP method and handler. It uses custom
+	// buckets based on the expected request duration.
+	duration := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "request_duration_seconds",
+			Help:    "A histogram of latencies for requests.",
+			Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
+		},
+		[]string{"handler", "method"},
+	)
+
+	prometheus.MustRegister(counter, duration)
+
+	return &PrometheusMiddleware{
+		counter:  counter,
+		duration: duration,
+	}
+
+}
+
+func (p *PrometheusMiddleware) Handler(next http.Handler) http.Handler {
+	middleware := promhttp.InstrumentHandlerDuration(p.duration.MustCurryWith(prometheus.Labels{"handler": "pull"}), promhttp.InstrumentHandlerCounter(p.counter,
+		next))
+
+	return middleware
+}
